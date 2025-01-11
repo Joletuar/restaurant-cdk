8b3ff9c6e67ffc809f9bd27d66fdb760d5da6a7d
Johan Tuarez <joletuar.dev@gmail.com>
2025-01-10 22:03:03 -0500

    feat: Implement SqsService for sending messages to SQS queues

---
+++ src/services/SqsService.ts
@@ -0,0 +1,40 @@
+import {
+  MessageAttributeValue,
+  SendMessageCommand,
+  SQSClient,
+  SQSClientConfig,
+} from '@aws-sdk/client-sqs';
+
+import * as crypto from 'crypto';
+
+export class SqsService {
+  sqsClient: SQSClient;
+
+  constructor(public config?: SQSClientConfig) {
+    if (config) this.sqsClient = new SQSClient(config);
+    else this.sqsClient = new SQSClient({});
+  }
+
+  async sendMessage<T>(params: {
+    data: T;
+    queueUrl: string;
+    messageGroupId?: string;
+    messageAttributes?: Record<string, MessageAttributeValue>;
+  }) {
+    const {
+      data,
+      queueUrl,
+      messageGroupId = crypto.randomUUID(),
+      messageAttributes,
+    } = params;
+
+    await this.sqsClient.send(
+      new SendMessageCommand({
+        QueueUrl: queueUrl,
+        MessageGroupId: messageGroupId,
+        MessageAttributes: messageAttributes,
+        MessageBody: JSON.stringify(data),
+      })
+    );
+  }
+}
