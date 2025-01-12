e77bf90a662d73d9c73e3f2148507dd2206d4064
Johan Tuarez <joletuar.dev@gmail.com>
2025-01-12 16:39:56 -0500

    feat: Implement createIngredient Lambda function with validation and DynamoDB integration

---
+++ src/lambdas/api-gateway-proxy-handlers/ingredients/createIngredient.ts
@@ -0,0 +1,39 @@
+import { zodValidator } from '@src/helpers/zodValidator';
+import { DynamoDbService } from '@src/services/DynamoDbService';
+import {
+  ApiProxyProcessor,
+  createApiGatewayProxyLambdaHandler,
+} from '@src/utils/cloud/createApiGatewayProxyLambdaHandler';
+import {
+  CreateIngredientEvent,
+  createIngredientEventSchema,
+} from './types/IngredientEvents';
+import { Ingredient } from '@src/types/Ingredient';
+import { envs } from '@src/config/envs';
+import { HttResponse } from '@src/utils/http/HttpResponse';
+import * as crypto from 'crypto';
+
+const dynamoService = new DynamoDbService();
+
+const processor: ApiProxyProcessor = async (event) => {
+  const parsedData = zodValidator<CreateIngredientEvent>(
+    event.body,
+    createIngredientEventSchema
+  );
+
+  const ingredient: Ingredient = {
+    ...parsedData,
+    id: crypto.randomUUID(),
+  };
+
+  await dynamoService.putData<Ingredient>({
+    tableName: envs.tables.ingredientsTableName,
+    data: ingredient,
+  });
+
+  return HttResponse.created();
+};
+
+export const handler = createApiGatewayProxyLambdaHandler({
+  processor,
+});
